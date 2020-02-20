2063b7674c02588690721aa7bf370a9b0c94e655
PureumJeon <50946414+PureumJeon@users.noreply.github.com>
2020-02-20 16:12:15 +0900

    ~2.7

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
@@ -8,8 +13,21 @@ const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
