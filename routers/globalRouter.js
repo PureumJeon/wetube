1a0c744ed0bae950ac2e618c645247af576679ae
PureumJeon <50946414+PureumJeon@users.noreply.github.com>
2020-03-12 19:39:58 +0900

    ~6.9

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,4 +1,5 @@
 import express from "express";
+import passport from "passport";
 import routes from "../routes";
 import { home, search } from "../controllers/videocontroller";
 import {
@@ -6,9 +7,12 @@ import {
   getJoin,
   postJoin,
   getLogin,
-  postLogin
+  postLogin,
+  githubLogin,
+  postGithubLogin,
+  getMe
 } from "../controllers/usercontroller";
-import { onlyPublic } from "../middlewares";
+import { onlyPublic, onlyPrivate } from "../middlewares";
 
 const globalRouter = express.Router();
 globalRouter.get(routes.join, onlyPublic, getJoin);
@@ -18,6 +22,16 @@ globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+globalRouter.get(routes.github, githubLogin);
+
+globalRouter.get(
+  routes.githubCallback,
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  postGithubLogin
+);
+
+globalRouter.get(routes.me, getMe);
 
 export default globalRouter;
