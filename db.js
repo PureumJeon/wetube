f63d2bcfe17eb5a854fc63cf0f45a1b7d1c187c6
PureumJeon <50946414+PureumJeon@users.noreply.github.com>
2020-03-21 14:17:17 +0900

    ~11.3.1

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL_PROD, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGO_URL, {
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✔ Connected to DB");
-const handleError = error => console.log(`❌ Error on DB Connection :${errer}`);
+const handleError = error => console.log(`❌ Error on DB Connection :${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleError);
