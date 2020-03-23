e09152dafdcf3fa1bd215d47a3f00a2c146a7517
PureumJeon <50946414+PureumJeon@users.noreply.github.com>
2020-03-23 20:13:43 +0900

    bug fix~3.23

--- src/init.js
+++ src/init.js
@@ -1,15 +1,14 @@
-import "@babel/polyfill";
-import dotenv from "dotenv";
-dotenv.config();
-
 import "./db";
 import app from "./app";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 import "./models/Video";
 import "./models/Comment";
 import "./models/User";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 400;
 
 const handleListening = () =>
   console.log(`✔ Listening on : http://localhost:${PORT}`);
